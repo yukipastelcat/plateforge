fb28c35e1e7f9ab3362281faff7c8d361f467f29
Kirill Sedunov <kirill.sedunov@outlook.com>
2023-05-13 23:41:08 +0000

    feat: setup vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,12 @@
 module.exports = {
+  plugins: ["vitest"],
   parser: "vue-eslint-parser",
   parserOptions: {
     parser: "@typescript-eslint/parser",
   },
-  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  extends: [
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:prettier/recommended",
+    "plugin:vitest/recommended",
+  ],
 };
