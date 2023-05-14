4b073e53cdcc5c0ade88aaf5319fc5fc0e5eb562
Kirill Sedunov <kirill.sedunov@outlook.com>
2023-05-14 15:55:39 +0000

    feat: add title to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Home page</h1>
+  <div class="py-16">
+    <h1 class="text-5xl text-center">
+      {{ t("title") }}
+    </h1>
+  </div>
 </template>
+
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+</script>
+
+<i18n lang="json">
+{
+  "en": {
+    "title": "Create custom license plates for your car models and games with ease."
+  }
+}
+</i18n>
